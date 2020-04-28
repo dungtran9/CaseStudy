0faf547973d3f89e347c66507645a320b3c7dc99
dungtran9 <63217229+dungtran9@users.noreply.github.com>
2020-04-28 17:26:43 +0700

    update

--- card.js
+++ card.js
@@ -23,159 +23,74 @@ class Card {
 
 cards = [];
 
-function createDesk() {
-    let arr = ["S", "C", "H", "D"];
-    for (let i = 1; i < 10; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            let name = i + arr[j];
-            let id = j * 10 + i;
-            let value = i
-            let img = name + ".jpg";
-            let card = new Card(id, name, value, img)
-            cards.push(card);
-        }
-    }
-}
-
-function showDesk() {
-    let desk = "";
-    for (let i = 0; i < cards.length; i++) {
-        if (cards[i].status) {
-            desk += cards[i].draw();
-        } else {
-            desk += cards[i].hidden();
-        }
-    }
-    document.getElementById("desk").innerHTML = desk;
-}
 
 function clickCard(id) {
-    for (let i = 0; i < cards.length; i++) {
-        if (cards[i].id === id) {
-            cards[i].status = true;
+    for (let i = 0; i < desk.cards.length; i++) {
+        if (desk.cards[i].id === id) {
+            desk.cards[i].status = true;
         }
     }
-    showDesk();
+    desk.show();
 
 }
-function createPlayer(player,num,desk) {
 
-     let abc ="";
-     for (let i=0;i<num;i++){
-         a = Math.floor(Math.random()*cards.length)
-     player.push(cards[a])
-     cards.slice(a,1)
-     abc +=cards[a].draw()
-         document.getElementById(desk).innerHTML =abc;
-     }
-}
+class Desk {
+    constructor() {
+        this.cards = [];
 
-createDesk();
-showDesk();
-let player1 = new Array();
-createPlayer(player1,3,"desk1");
-let player2 = new Array();
-createPlayer(player2,3,"desk2")
-function checkWin() {
-    point1 = (player1[0].value + player1[1].value + player1[2].value) % 10;
-    point2 = (player2[0].value + player2[1].value + player2[2].value) % 10;
-    if (point1 == 0) {
-        point1 = 10
     }
-     ;
-    if (point2 == 0) {
-        point2 = 10
-     }
-     setTimeout(checkWin, 500)
 
-        if (point1 > point2) {
-            document.getElementById("result").innerHTML = "Player1 win"
-        } else if (point1 < point2) {
-             document.getElementById("result").innerHTML = "Player2 win"
-         } }
+    create() {
+        let arr = ["S", "C", "H", "D"];
+        for (let i = 1; i < 10; i++) {
+            for (let j = 0; j < arr.length; j++) {
+                let name = i + arr[j];
+                let id = j * 10 + i;
+                let value = i
+                let img = name + ".jpg";
+                let card = new Card(id, name, value, img)
+                this.cards.push(card);
+            }
+        }
+    }
 
-// function showShare(x, y, z, a, b, c) {
-//     let player1 = "";
-//     player1 = cards[x].draw() + cards[y].draw() + cards[z].draw()
-//     document.getElementById("player1").innerHTML = player1;
-//     let player2 = "";
-//     player2 = cards[a].draw() + cards[b].draw() + cards[c].draw()
-//     document.getElementById("player2").innerHTML = player2;
-//
-// }
-//
-// function shareCard() {
-//     let num = new Array();
-//     for (let i = 0; i < 6; i++) {
-//         let number1 = Math.floor(Math.random() * 35);
-//         if (i == 0) {
-//             num.push(number1);
-//         } else {
-//             checkIsset(num, number1);
-//         }
-//     }
-//
-//     function checkIsset(num, number) {
-//         if (num.indexOf(number) === -1) {
-//             num.push(number);
-//             return number
-//         } else {
-//             let number2 = Math.floor(Math.random() * 35);
-//             checkIsset(num, number2);
-//         }
-//     }
-//
-//     showShare(num[0], num[1], num[2], num[3], num[4], num[5])
-//     point1 = (cards[num[0]].value + cards[num[1]].value + cards[num[2]].value) % 10;
-//     point2 = (cards[num[3]].value + cards[num[4]].value + cards[num[5]].value) % 10;
-//     if (point1 == 0) {
-//         point1 = 10
-//     }
-//     ;
-//     if (point2 == 0) {
-//         point2 = 10
-//     }
-//     setTimeout(checkWin, 500)
-//
-//     function checkWin() {
-//
-//         if (point1 > point2) {
-//             document.getElementById("result").innerHTML = "Player1 win"
-//         } else if (point1 < point2) {
-//             document.getElementById("result").innerHTML = "Player2 win"
-//         } else if (point1 === point2) {
-//             if (cards[num[0]].id > cards[num[3]].id &&
-//                 cards[num[0]].id > cards[num[4]].id &&
-//                 cards[num[0]].id > cards[num[5]].id) {
-//                 document.getElementById("result").innerHTML = "Player1 win"
-//             }
-//             if (cards[num[1]].id > cards[num[3]].id &&
-//                 cards[num[1]].id > cards[num[4]].id &&
-//                 cards[num[1]].id > cards[num[5]].id) {
-//                 document.getElementById("result").innerHTML = "Player1 win"
-//             }
-//             if (cards[num[2]].id > cards[num[3]].id &&
-//                 cards[num[2]].id > cards[num[4]].id &&
-//                 cards[num[2]].id > cards[num[5]].id) {
-//                 document.getElementById("result").innerHTML = "Player1 win"
-//             }
-//             if (cards[num[3]].id > cards[num[0]].id &&
-//                 cards[num[3]].id > cards[num[1]].id &&
-//                 cards[num[3]].id > cards[num[2]].id) {
-//                 document.getElementById("result").innerHTML = "Player2 win"
-//             }
-//             if (cards[num[4]].id > cards[num[0]].id &&
-//                 cards[num[4]].id > cards[num[1]].id &&
-//                 cards[num[4]].id > cards[num[2]].id) {
-//                 document.getElementById("result").innerHTML = "Player2 win"
-//             }
-//             if (cards[num[5]].id > cards[num[0]].id &&
-//                 cards[num[5]].id > cards[num[1]].id &&
-//                 cards[num[5]].id > cards[num[2]].id) {
-//                 document.getElementById("result").innerHTML = "Player2 win"
-//             }
-//         }
-//     }
-// }
+    show() {
+        let desk = "";
+        for (let i = 0; i < this.cards.length; i++) {
+            if (this.cards[i].status) {
+                desk += this.cards[i].draw();
+            } else {
+                desk += this.cards[i].hidden();
+            }
+        }
+        document.getElementById("desk").innerHTML = desk;
+    }
+}
+
+let desk = new Desk()
+desk.create();
+desk.show()
+class Player {
+   constructor(name) {
+       this.name = name;
+       this.cards =[];
+       this.money = 1000;
+   }
+
+   getScore(){
+       let sum = 0;
+       for (let i = 0; i <this.cards.length ; i++) {
+           sum+=this.cards[i].value;
+       }
+       sum = sum%10==0?10:sum%10;
+       console.log(sum%10)
+       return sum;
+   }
+   addCards(a,b,c){
+       this.cards.push(a,b,c);
+   }
+}
+let player1 = new Player("Dung");
+player1.addCards(desk.cards[1],desk.cards[2],desk.cards[30]);
 
 
